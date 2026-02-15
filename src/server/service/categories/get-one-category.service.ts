c52564107cd66f47a87979b40d55cb8a15749ac3
ahmdriyo <ahmdriyo.tbn.com>
2026-02-15 15:47:32 +0800

    feat: implement getOneCategory service for fetching category by ID

--- src/server/service/categories/get-one-category.service.ts
+++ src/server/service/categories/get-one-category.service.ts
@@ -1,25 +1,23 @@
 import { NextResponse } from "next/server";
-import { findCategoryById } from "../../repositories/categories/find-by-id-categories.repository";
+import { findCategoryById } from "../../repositories/categories/find-by-id-category.repository";
 
 export async function getOneCategory(id: string) {
   try {
     const category = await findCategoryById(id);
-
     if (!category) {
       return NextResponse.json(
         { message: "Category not found" },
         { status: 404 },
       );
     }
-
     return NextResponse.json({
-      message: "Category fetched!",
+      message: "Category fetched successfully!",
       data: category,
     });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { message: "Failed to fetch category" },
+      { message: "Failed to fetch category." },
       { status: 500 },
     );
   }
